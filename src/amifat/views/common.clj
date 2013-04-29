1db93efa109d9c3225dd9a4368793ea0f22d20e1
Sergi Castro <sergikstro@gmail.com>
2013-04-29 13:25:38 +0200

    formating travis

--- src/amifat/views/common.clj
+++ src/amifat/views/common.clj
@@ -5,10 +5,10 @@
 (defpartial layout [& content]
             (html5
               [:head
-               [:title "Am I fat?"]
-               ;(include-css "/css/reset.css")
-			   ;(include-css "/css/noir.css")]
-			   (include-css "/css/basic.css")]
+                [:title "Am I fat?"]
+                ;(include-css "/css/reset.css")
+	        ;(include-css "/css/noir.css")]
+		(include-css "/css/basic.css")]
               [:body
-               [:div#wrapper
+                [:div#wrapper
                 content]]))
